cf1a93838bc953966fa773abbdbcaa4ddbb43808
Adhitya Balamurugan <aadhityabalamurugan@gmail.com>
2019-07-27 15:10:44 +0530

    Add files via upload

--- nestedeaser.py
+++ nestedeaser.py
@@ -1,11 +1,12 @@
 """A recursive Python function"""
 
-def recursive_func(the_list,level):
+def recursive_func(the_list,indent=False,level=0):
 	for each_item in the_list :
 		if isinstance (each_item,list):
 			recursive_func(each_item,level+1)
 		else :
-			for tab_stop in range(level):
-				print("\t",end='') #for tab spaces to show recursion
+			if indent :
+				for tab_stop in range(level):
+					print("\t",end='') #for tab spaces to show recursion
 			print(each_item)
 """A simple recursive function to print nested lists""" 
