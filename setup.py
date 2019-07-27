645a289dfc84488a1f5fcbadcc56d58c294996a8
Adhitya Balamurugan <aadhityabalamurugan@gmail.com>
2019-07-27 14:42:12 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name         = 'NestedEase',
+	version      = '1.1.0',
+	py_modules   = ['nestedeaser'],
+	author       = 'WolvesNRavens',
+	author_email = 'wolvesnravens1542@gmail.com',
+	#url          = 'http://www.headfirstlabs.com',
+	description  = 'A Recursive function for nested lists',
+	)
