512b0b33dcf33091a63d33f747c5b2b436e23ba9
Adhitya Balamurugan <aadhityabalamurugan@gmail.com>
2019-07-28 08:08:13 +0530

    Update nestedeaser.py

--- nestedeaser.py
+++ nestedeaser.py
@@ -3,7 +3,7 @@
 def recursive_func(the_list,indent=False,level=0):
 	for each_item in the_list :
 		if isinstance (each_item,list):
-			recursive_func(each_item,level+1)
+			recursive_func(each_item,indent,level+1)
 		else :
 			if indent :
 				for tab_stop in range(level):
